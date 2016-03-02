0482687e11bf03d729edea1c511d898596257c3b
trinile <trinile@gmail.com>
2016-03-02 14:55:17 -0800

    Queue Functional Pattern pass spec to report size of 2 after adding two items.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,19 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    items++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return items;
   };
 
   return someInstance;
