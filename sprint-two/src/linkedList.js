10ed31ccf3e748339a6d659f6b2f8f1c14f6b578
trinile <trinile@gmail.com>
2016-03-03 11:12:52 -0800

    AddTail and RemoveHead methods passing for LinkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,30 @@
 var LinkedList = function() {
   var list = {};
+  var nodes = 0;
+  // var headCounter = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    list[nodes] = new Node(value);
+    if (nodes === 0) {
+      list.head = list[nodes];
+    }
+    list.tail = list[nodes];
+    // list.head = null;  
+    nodes++;
+    return list.tail.value;
   };
 
   list.removeHead = function() {
+    var head = list.head;
+    nodes--;
+    for (var i = 0; i < nodes; i++) {
+      var nextKey = i + 1;
+      list[i] = list[nextKey];
+    }
+    list.head = list[0];
+    delete list[nodes];
   };
 
   list.contains = function(target) {
