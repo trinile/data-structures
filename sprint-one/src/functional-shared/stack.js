c43c66227d65b4a369e7f8b0e025968573d2be73
trinile <trinile@gmail.com>
2016-03-02 16:11:23 -0800

    Functional-shared Pattern stack to pass size of zero for a new stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,17 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.size = stackMethods.stackSize;
+  
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  stackSize: function() {
+    return 0;
+  }
+};
 
 
