3972dc788e71e21d5ab0b2f8f460b37c2da22287
trinile <trinile@gmail.com>
2016-03-03 10:05:19 -0800

    Complete pseudoclassical pattern for stack shared behavior

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.items = 0;
+  this.storage = {};
 };
+
+Stack.prototype.size = function() {
+  return this.items = this.items < 0 ? 0 : this.items;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.items] = value;
+  this.items++;
+};
+
+Stack.prototype.pop = function() {
+  this.items--;
+  var last = this.storage[this.items];
+  delete this.storage[this.items];
+  return last;
+};
+
+var stack = new Stack();
 
 
