67665e231e386700d989844c262952181469a152
trinile <trinile@gmail.com>
2016-03-02 16:18:41 -0800

    Functional Shared pattern to pass size of 2 after addign two items.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,15 +2,19 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
-
+  someInstance.items = 0;
   someInstance.size = stackMethods.stackSize;
-  
+  someInstance.push = stackMethods.stackPush;
   return someInstance;
 };
 
 var stackMethods = {
   stackSize: function() {
-    return 0;
+    return this.items;
+  },
+
+  stackPush: function() {
+    this.items++;
   }
 };
 
