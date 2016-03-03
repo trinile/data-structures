aac22a94fc7875083d45523bbabd008cdcc87b4e
trinile <trinile@gmail.com>
2016-03-03 14:09:43 -0800

    Refactor LinkedList to remove for-loops

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,37 +1,31 @@
 var LinkedList = function() {
   var list = {};
-  var nodes = 0;
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
-    list[nodes] = new Node(value);
-    if (nodes === 0) {
-      list.head = list[nodes];
+    if (list.head === null) {
+      list.head = new Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = new Node(value);
+      list.tail = list.tail.next;
     }
-    list.tail = list[nodes];
-    // list.head = null;  
-    nodes++;
-    return list.tail.value;
   };
 
   list.removeHead = function() {
     var head = list.head;
-    nodes--;
-    for (var i = 0; i < nodes; i++) {
-      var nextKey = i + 1;
-      list[i] = list[nextKey];
-    }
-    list.head = list[0];
-    delete list[nodes];
+    list.head = list.head.next;
     return head.value;
   };
 
   list.contains = function(target) {
-    for (var i = 0; i < nodes; i++) {
-      if (target === list[i].value) {
+    var node = list.head;
+    while (node !== null) {
+      if (node.value === target) {
         return true;
       }
+      node = node.next;
     }
     return false;
   };
@@ -50,4 +44,8 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ list.addToTail : constant time complexity.
+ list.removeHead : constant time complexity. 
+ list.contains : linear time complexity. Looping through entire linked list to determine if target exists.
+
  */
