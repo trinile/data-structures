e0d4c7e0166685b0108bde37ceceb508ec6dee84
trinile <trinile@gmail.com>
2016-03-04 12:30:28 -0800

    Saving work in progress re: hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -10,12 +10,26 @@ HashTable.prototype.insert = function(k, v) {
   //index where key-value pair belongs to 
   //add that key-value pair to a bucket array IN that storage array
   //key-value pair is added at tuple index 0: key, index 1: value
-  this._storage.set(index, v); //storage[index] = value
+  if (!this._storage.get(index)) {
+    this._storage.set(index, []);
+  }
+  this._storage.get(index).push([k, v]);
+  console.log(this._storage.get(index));
+    
+
+    var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] !== k) {
+      this._storage.get(index).push([k, v]);
+    } 
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  // return this._storage.get(index);
+  bucketIndex = index[0];
+  return this._storage.get(bucketIndex);
 };
 
 HashTable.prototype.remove = function(k) {
