88e6682734b711d8ebf24b7102745aedd7504e5e
trinile <trinile@gmail.com>
2016-03-04 11:24:50 -0800

    HashTable insert and retrieve functions working for initial tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,15 +2,20 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storage = LimitedArray(this._limit); //{get, set, each methods}
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit); //index in storage array
+  //index where key-value pair belongs to 
+  //add that key-value pair to a bucket array IN that storage array
+  //key-value pair is added at tuple index 0: key, index 1: value
+  this._storage.set(index, v); //storage[index] = value
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
