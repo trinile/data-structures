def1fe05d9e4630e26f96c0b1a371f55c72fd309
trinile <trinile@gmail.com>
2016-03-04 10:57:51 -0800

    Set sprint to pass specs

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,21 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage = this._storage || [];
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.indexOf(item) !== -1;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[this._storage.indexOf(item)];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: constant
+ * contains: linear
+ * remove: linear
  */
