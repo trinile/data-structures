fc78b7dab94b83fbdf9ef54141465c3c3151f431
trinile <trinile@gmail.com>
2016-03-04 18:07:48 -0800

    BinarySearchTree insert function to pass initial test specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -2,15 +2,31 @@ var BinarySearchTree = function(value) {
   var newTree = {};
   newTree.value = value;
   _.extend(newTree, BinarySearchMethods);
+  newTree.left = null;
+  newTree.right = null;
   return newTree;
 };
 
 BinarySearchMethods = { };
 
-BinarySearchMethods.insert = function() { 
+BinarySearchMethods.insert = function(value) { 
+  if (value < this.value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  } else if (value > this.value) {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  }
 };
 
 BinarySearchMethods.contains = function() {
+
 };
 
 BinarySearchMethods.depthFirstLog = function() {
