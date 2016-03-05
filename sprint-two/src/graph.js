757f1599ad2c6660a57cd32d3427d9f585cc49d9
trinile <trinile@gmail.com>
2016-03-04 18:03:46 -0800

    Refactor graph sprint to add edges as a property of each node

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,23 +3,19 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
-  this.nodes = null;
-  this.edges = {};
+  this.nodes = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  if (this.nodes === null) {
-    this.nodes = {};
-  }
-  this.nodes[node] = node;
+  this.nodes[node] = {};
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return this.nodes[node] === node;
+  return this.nodes.hasOwnProperty(node);
 };
 
 // ------------------------
@@ -31,31 +27,28 @@ Graph.prototype.removeNode = function(node) {
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var edge = fromNode < toNode ? JSON.stringify(arguments[0], arguments[1]) : JSON.stringify(arguments[1], arguments[0]);
-  return !!(this.edges[edge]);
+  return this.nodes[fromNode].hasOwnProperty(toNode);
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  if (this.contains(this.nodes.fromNode) && this.contains(this.nodes.toNode)) {
-    var edge = fromNode < toNode ? JSON.stringify(arguments[0], arguments[1]) : JSON.stringify(arguments[1], arguments[0]);
-    this.edges[edge] = true;
-  }
+  this.nodes[fromNode][toNode] = true;
+  this.nodes[toNode][fromNode] = true;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) { 
-  var edge = fromNode < toNode ? JSON.stringify(arguments[0], arguments[1]) : JSON.stringify(arguments[1], arguments[0]);
-  delete this.edges[edge];
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for (node in this.nodes) {
-    cb(this.nodes[node]);
+    cb(node);
   }
 };
 
