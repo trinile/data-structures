b0f9b647104e9d0ad1866d28f1972b914797ff01
trinile <trinile@gmail.com>
2016-03-04 16:13:20 -0800

    binarySearchTree with functions: insert, contains, and depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,20 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  _.extend(newTree, BinarySearchMethods);
+  return newTree;
 };
 
+BinarySearchMethods = { };
+
+BinarySearchMethods.insert = function() { 
+};
+
+BinarySearchMethods.contains = function() {
+};
+
+BinarySearchMethods.depthFirstLog = function() {
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
